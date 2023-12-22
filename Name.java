e8165fcf0a22898dcf90ddd5272c144daccb4e62
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-22 13:09:22 +0800

    Name+Swan update
    i add Name and Swan class

---
+++ Name.java
@@ -0,0 +1,11 @@
+public class Name {
+
+  public static void main(String[] args) {
+    String First = "theodore ";
+    String Last = " Moose";
+    String Full = First + Last;
+
+    System.out.println(Full);
+
+  }
+}
