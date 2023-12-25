1ef3870ecda540ea82a67ee1c27edd2639b14d0a
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-25 23:10:21 +0800

    Main+while updt

--- Main.java
+++ Main.java
@@ -5,5 +5,13 @@ public class Main {
   public static void main(String[] args) {
     Random r = new Random();
     System.out.println(r.nextInt(15));
+
+    String name = "bro";
+    //int result = name.indexOf("b");
+    //boolean result = name.isEmpty();
+    //String result = name.toUpperCase();
+    //String add = "! ";
+    //boolean result = name.equalsIgnoreCase("bro");
+    System.out.println();
   }
 }
