dbc9be1f8b9f50c8d5ef30b4a6c65433ea8d5365
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-25 22:01:03 +0800

    switch upd

--- Swan.java
+++
@@ -1,9 +0,0 @@
-public class Swan {
-  int numberEggs;
-
-  public static void main(String[] args) {
-    Swan mother = new Swan();
-    mother.numberEggs = 1;
-
-  }
-}
---
+++ day.java
@@ -0,0 +1,17 @@
+public class day {
+  
+  public static void main(String[] args) {
+    
+    String day = "Sunday";
+
+    switch (day) {
+      case "Sunday":System.out.println("it is Sunday! ");
+      break;
+      case "Monday":System.out.println("it is Monday! ");
+      break;
+    
+      default:System.out.println("That is not a day! ");
+        break;
+    }
+  }
+}
