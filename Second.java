996dd2fbb5c0bff839f37737f7682b89cf5fe57d
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-25 13:47:47 +0800

    Random update 0.0.1
    random update .1

---
+++ Second.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Second {
+  
+  public static void main(String[] args){
+
+    Random r = new Random();
+
+    //int x = r.nextInt(10);
+    boolean y = r.nextBoolean();
+    System.out.println(y);
+  }
+}
