4066674aca5c48a331d68a87dcebccc9ca263bfd
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-26 19:32:43 +0800

    changes

--- Scanner1.java
+++ Scanner1.java
@@ -5,5 +5,7 @@ public static void main(String[] args) {
   String name = JOptionPane.showInputDialog("Enter name");
   JOptionPane.showMessageDialog(null,"you are "+name);
 
+  int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you? "));
+  JOptionPane.showMessageDialog(null,"You are "+age+" years old");
   }
 }
