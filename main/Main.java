1f829b3d5ebcbe9b620544d210dd4749c0147f46
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-26 20:08:28 +0800

    changes

--- main/Main.java
+++ main/Main.java
@@ -2,6 +2,10 @@ package main;
 
 public class Main {
     public static void main(String[] args){
+     int x = 10;
+     int y = 25;
 
+     int z = Math.max(x,y);
+        System.out.println(z);
     }
 }
