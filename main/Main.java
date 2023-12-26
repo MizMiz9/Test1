a9504bf7a032a30cffa5bf1c4a6d31cabc058358
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-26 20:15:51 +0800

    update 8:15pm

--- main/Main.java
+++ main/Main.java
@@ -1,11 +1,17 @@
 package main;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args){
      int x = 10;
      int y = 25;
 
-     int z = Math.max(x,y);
+     int z = Math.min(x,y);
         System.out.println(z);
+     Random r = new Random();
+     int g = r.nextInt(15);
+     System.out.println(g);
+
     }
 }
