ca5c99522cf177b1b3a73f89be5f371e94348c43
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-26 18:22:34 +0800

    Create Scanner1.java

---
+++ Scanner1.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class Scanner1{
+
+public static void main(String[] args) {
+  String name = JOptionPane.showInputDialog("Enter name");
+  JOptionPane.showMessageDialog(null,"you are "+name);
+
+  }
+}
