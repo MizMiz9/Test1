5bbc88059dec307a9f61046b54fbe93d0a63e540
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-26 19:45:27 +0800

    changes

--- main/Main.java
+++ main/Main.java
@@ -2,6 +2,12 @@ package main;
 
 public class Main {
      public static void main(String[]args){
+          double x = 11.0;
+          double y = 10.3;
+
+          double z = Math.min(x,y);
+          System.out.println(z);
+
 
      }
 }
