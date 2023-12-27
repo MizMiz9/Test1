4644493cb3f9b7dc434f0c97df29f26db7f59970
MizMiz9 <152782830+MizMiz9@users.noreply.github.com>
2023-12-27 15:04:27 +0800

    test class java

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+  public static void main(String[] args) {
+   sum();
+  }
+  private static void sum(){
+    int x = 10;
+    int y = 25;
+    int z = x + y;
+    System.out.println(z);
+  }
+}
